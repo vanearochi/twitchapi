8fdc1ddef6cd1c1aac173bb141fffff1fc6a7062
Vane Arochi <vanearochi@gmail.com>
2015-11-11 16:48:39 -0800

    tabs online and offline added

--- media/js/twitchapi.js
+++ media/js/twitchapi.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	console.log("Welcome to twitchapi by Vane")
-	var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff"];
+	var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","medrybw"];
 	html=""
 	// var userInfo = function(json){
 	// 	console.log(json)
@@ -10,18 +10,19 @@ $(document).ready(function(){
 	// }
 
 	var userInfo = function(arr){
+
 		var userData = function(json){
 			//console.log("test1"+json.display_name
 			//console.log(html)
 			status(json.name, json.bio, json.logo, json.display_name)
-			displayUserData(json.name,json.logo, json.display_name)
-			
+			// displayUserData(json.name,json.logo, json.display_name)
+			console.log(json)
 			
 			
 		}
 
-		for (var i = 0; i < arr.length-1; i++) {
-			
+		for (var i = 0; i <= arr.length-1; i++) {
+			console.log(arr)
 			$.ajax({
 	     		url: "https://api.twitch.tv/kraken/users/"+arr[i],
 	     		success: userData,
@@ -65,25 +66,35 @@ $(document).ready(function(){
 
 	// 
 
-	var displayUserData = function(userName, logo, displayName){
-		$("."+userName).html("<img src="+logo+">"+"<br>"+"<p>"+displayName+"</p>")
-		//$("."+userName).html("<div>"+ bio+"</div>")
+	// var displayUserData = function(userName, logo, displayName){
+	// 	if(logo===null){
+	// 		$("."+userName).html("<img src=>"+"<br>"+"<p>"+displayName+"</p>")
+	// 	}
+	// 	else{
+	// 	$("."+userName).html("<img src="+logo+">"+"<br>"+"<p>"+displayName+"</p>")
+	// 	}
+	// 	//$("."+userName).html("<div>"+ bio+"</div>")
 
-	}
+	// }
 
 	var displayStatus = function(status,userName,bio, logo, displayName){
 		if(status===null){
-			$("."+userName).html("<img src="+logo+">"+"<br>"+"<p>"+displayName+"</p>"+"<p class=status>bu</p>")
+			$("."+userName).html("<img src="+logo+">"+"<div class='userName'><a href='http://www.twitch.tv/"+userName+"/profile'>"+displayName+"</a></div>"+"<div class='offline'></div>")
+			$("."+userName+"2").html("<img src="+logo+">"+"<div class='userName'><a href='http://www.twitch.tv/"+userName+"/profile'>"+displayName+"</a></div>"+"<div class='offline'></div>")
 		}
 		else{
-			$("."+userName).html("<img src="+logo+">"+"<br>"+"<p>"+displayName+"</p>"+"<p>"+bio+"</p>")
+			$("."+userName).html("<img src="+logo+">"+"<p>"+displayName+"</p>"+"<div class=bio>"+bio+"</div>"+"<div class='online'></div>")
+			$("."+userName+"1").html("<img src="+logo+">"+"<p>"+displayName+"</p>"+"<div class='online'></div>"+"<div class=bio>"+bio+"</div>")
 		}
 	}
 
-	var events = function(){
-		$(".freecodecamp").on("click"), function(){console.log(json.name)}
+	
+		// $(".all").on("click", function(){
+		//  	$("."+).html("<a href='http://www.twitch.tv/terakilobyte/profile'></a>")
+		//  });
 
-	}
+	
+	
 	
 
 
